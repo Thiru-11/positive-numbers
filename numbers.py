e88cbabbca738a60119098348ad2fe8fa21e1787
Thiru-11 <79251410+Thiru-11@users.noreply.github.com>
2021-05-26 18:13:01 +0530

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,11 @@
+list1 = [11,12,-1,-3,3,4]
+list2=[1,2,3,-4,-5]
+num = 0
+  
+# using while loop     
+while(num < len(list1)):
+      
+# checking condition
+    if list1[num]>= 0:
+        print(list1[num],end = " ")
+    num += 1
